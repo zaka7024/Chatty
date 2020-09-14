cdcdcf7ee75e90feb5ec4e9e1b665a2ee8dcffaf
zaka7024 <38314622+zaka7024@users.noreply.github.com>
2020-09-14 10:13:15 +0300

    add navigation to main fragments

--- app/src/main/java/com/freshie/chatty/MainActivity.kt
+++ app/src/main/java/com/freshie/chatty/MainActivity.kt
@@ -2,10 +2,45 @@ package com.freshie.chatty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.fxn.OnBubbleClickListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        navController = findNavController(R.id.myNavHostFragment)
+        Navigation.setViewNavController(bubbleTabBar, navController)
+        setBubbleListeners()
+    }
+
+    // navigate to main fragments
+    private fun setBubbleListeners() {
+        bubbleTabBar.addBubbleListener(object : OnBubbleClickListener {
+            override fun onBubbleClick(id: Int) {
+                when(id){
+                    R.id.nav_search -> {
+                        navController.navigate(R.id.discoverFragment)
+                    }
+                    R.id.nav_home -> {
+                        navController.navigate(R.id.homeFragment)
+                    }
+                    R.id.nav_profile -> {
+                        navController.navigate(R.id.profileFragment)
+                    }
+                }
+            }
+        })
     }
 }
